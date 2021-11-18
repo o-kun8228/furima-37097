21a198c9df75c4c4d2e6abce061590aae8e99cdb
o-kun8228 <92617996+o-kun8228@users.noreply.github.com>
2021-11-18 12:29:15 +0900

    ユーザー管理

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,8 +6,8 @@ class User < ApplicationRecord
 
 
   validates :nickname, presence: true
-  validates :encrypted_password,:password,:password_confirmation,length:{minimum:6},format:{with: /(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]{6,}/}
-  with_options presence: true, format: {with: /\A[ぁ-んァ-ン一-龥]/} do
+  validates :password,format:{with:/\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i }
+  with_options presence: true, format: {with: /\A(?:\p{Hiragana}|\p{Katakana}|[ー－]|[一-龠々])+\z/} do
       validates :family_name
       validates :first_name
     end
@@ -17,8 +17,6 @@ class User < ApplicationRecord
       validates :family_name_zenkaku_katakana
       validates :first_name_zenkaku_katakana
     end
-
-    validates :password, presence: true, format: {with:/\A[a-zA-Z0-9]+\z/}
 
 
   validates :birthday, presence: true
