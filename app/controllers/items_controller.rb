f8ecb1de6bd4cc76921afe84b8a813a15022fac5
o-kun8228 <92617996+o-kun8228@users.noreply.github.com>
2021-11-20 12:27:33 +0900

    コントローラー

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,13 @@
 class ItemsController < ApplicationController
+
+  before_action :authenticate_user!,only:[:new]
+
   def index
   #@items = Item.all
   end
+
+  def new
+    @item = Item.new
+    end
+
 end
