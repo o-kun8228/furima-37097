7c03a0a70ef3af3f5c487e98bce69ecb20c343b1
o-kun8228 <92617996+o-kun8228@users.noreply.github.com>
2021-11-23 16:07:10 +0900

    rubocopの修正

--- app/models/category.rb
+++ app/models/category.rb
@@ -11,11 +11,9 @@ class Category < ActiveHash::Base
     { id: 9, name: 'スポーツ・レジャー' },
     { id: 10, name: 'ハンドメイド' },
     { id: 11, name: 'その他' }
-    
 
   ]
 
   include ActiveHash::Associations
   has_many :items
-
-  end
+end
